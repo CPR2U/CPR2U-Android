09c802f6584be41f8de088e9e02b742ef57406c5
chaeyeong <shimcy2010@gmail.com>
2023-03-08 05:09:40 +0900

    [FEAT] #4 - sms code edittext 자동 넘어가기 설정

--- app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginPhoneNumberCheckFragment.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginPhoneNumberCheckFragment.kt
@@ -3,6 +3,9 @@ package com.example.cpr2u_android.presentation.auth
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import com.example.cpr2u_android.R
 import com.example.cpr2u_android.databinding.FragmentLoginPhoneNumberCheckBinding
 import com.example.cpr2u_android.presentation.base.BaseFragment
@@ -12,6 +15,7 @@ class LoginPhoneNumberCheckFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initClickEvent()
+        initSmsCodeEvent()
     }
 
     private fun initClickEvent() {
@@ -20,6 +24,26 @@ class LoginPhoneNumberCheckFragment :
                 Intent(requireContext(), SignUpActivity::class.java)
             startActivity(intent)
             requireActivity().finishAffinity()
+        }
+    }
+
+    private fun initSmsCodeEvent() {
+        val smsCode: List<EditText> =
+            listOf(binding.tvSmsCode1, binding.tvSmsCode2, binding.tvSmsCode3, binding.tvSmsCode4)
+
+        for (i in 0..3) {
+            if (i in 0..2) {
+                smsCode[i].addTextChangedListener {
+                    if (smsCode[i].length() > 0) {
+                        smsCode[i + 1].requestFocus()
+                    }
+                }
+            }
+            smsCode[i].onFocusChangeListener = OnFocusChangeListener { view, hasFocus ->
+                if (hasFocus) {
+                    smsCode[i].text.clear()
+                }
+            }
         }
     }
 }
