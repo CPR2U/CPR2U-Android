1dfefd67a6c9eda1100cbe151e995e79e4709502
chaeyeong <shimcy2010@gmail.com>
2023-03-09 23:05:02 +0900

    [FEAT] #4 - 로그인 로직

--- app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginPhoneNumberCheckFragment.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginPhoneNumberCheckFragment.kt
@@ -5,35 +5,55 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.widget.EditText
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
 import com.example.cpr2u_android.R
 import com.example.cpr2u_android.databinding.FragmentLoginPhoneNumberCheckBinding
 import com.example.cpr2u_android.presentation.base.BaseFragment
 
 class LoginPhoneNumberCheckFragment :
     BaseFragment<FragmentLoginPhoneNumberCheckBinding>(R.layout.fragment_login_phone_number_check) {
+    private val signInViewModel: SignInViewModel by activityViewModels()
+    private lateinit var smsCode: List<EditText>
+    var smsCodeStr: String = ""
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        smsCode =
+            listOf(binding.tvSmsCode1, binding.tvSmsCode2, binding.tvSmsCode3, binding.tvSmsCode4)
+
+        initPhoneNumber()
         initClickEvent()
         initSmsCodeEvent()
     }
 
+    private fun initPhoneNumber() {
+        val phoneNumber = arguments?.getString("phoneNumber").toString()
+        binding.phoneNumber = phoneNumber
+    }
+
     private fun initClickEvent() {
         binding.tvConfirm.setOnClickListener {
-            val intent =
-                Intent(requireContext(), SignUpActivity::class.java)
-            startActivity(intent)
-            requireActivity().finishAffinity()
+            smsCode.forEach {
+                smsCodeStr += it.text.toString()
+            }
+
+            if (smsCodeStr.length < 4) {
+                Toast.makeText(requireContext(), "코드를 모두 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent =
+                    Intent(requireContext(), SignUpActivity::class.java)
+                startActivity(intent)
+                requireActivity().finishAffinity()
+            }
         }
     }
 
     private fun initSmsCodeEvent() {
-        val smsCode: List<EditText> =
-            listOf(binding.tvSmsCode1, binding.tvSmsCode2, binding.tvSmsCode3, binding.tvSmsCode4)
-
         for (i in 0..3) {
-            if (i in 0..2) {
-                smsCode[i].addTextChangedListener {
+            smsCode[i].addTextChangedListener {
+                if (i in 0..2) {
                     if (smsCode[i].length() > 0) {
                         smsCode[i + 1].requestFocus()
                     }
--- app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginPhoneNumberFragment.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginPhoneNumberFragment.kt
@@ -2,6 +2,8 @@ package com.example.cpr2u_android.presentation.auth
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cpr2u_android.R
 import com.example.cpr2u_android.databinding.FragmentLoginPhoneNumberBinding
@@ -9,16 +11,32 @@ import com.example.cpr2u_android.presentation.base.BaseFragment
 
 class LoginPhoneNumberFragment :
     BaseFragment<FragmentLoginPhoneNumberBinding>(R.layout.fragment_login_phone_number) {
+    private val signInViewModel: SignInViewModel by activityViewModels()
+    private var bundle = Bundle()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initClickEvent()
+        observePhoneNumber()
+    }
+
+    private fun observePhoneNumber() {
+        binding.etNumber.addTextChangedListener {
+            binding.tvSend.isSelected = !it.isNullOrEmpty()
+        }
     }
 
     private fun initClickEvent() {
         binding.tvSend.setOnClickListener {
-            findNavController().navigate(R.id.action_loginPhoneNumberFragment_to_loginPhoneNumberCheckFragment)
+            if (binding.tvSend.isSelected) {
+                bundle = Bundle().apply {
+                    putString("phoneNumber", binding.etNumber.text.toString())
+                }
+                findNavController().navigate(
+                    R.id.action_loginPhoneNumberFragment_to_loginPhoneNumberCheckFragment,
+                    bundle,
+                )
+            }
         }
     }
 }
