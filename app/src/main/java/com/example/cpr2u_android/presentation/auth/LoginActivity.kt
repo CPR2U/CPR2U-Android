df086dbeab39109ad4f4a9844c203e1151eef22e
chaeyeong <shimcy2010@gmail.com>
2023-03-09 23:01:17 +0900

    [CHORE] #4 - 쓰레기파일

--- app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginActivity.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/auth/LoginActivity.kt
@@ -14,6 +14,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initNavigation()
+
     }
 
     private fun initNavigation() {
