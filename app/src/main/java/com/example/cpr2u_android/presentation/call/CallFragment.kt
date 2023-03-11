adc3488e2ad1d9c9064dc67a1f904d19c2fb3392
chaeyeong <shimcy2010@gmail.com>
2023-03-11 15:30:06 +0900

    [FEAT] $9 - 네비게이션 연결 fragment 생성

---
+++ app/src/main/java/com/example/cpr2u_android/presentation/call/CallFragment.kt
@@ -0,0 +1,13 @@
+package com.example.cpr2u_android.presentation.call
+
+import android.os.Bundle
+import android.view.View
+import com.example.cpr2u_android.R
+import com.example.cpr2u_android.databinding.FragmentCallBinding
+import com.example.cpr2u_android.presentation.base.BaseFragment
+
+class CallFragment : BaseFragment<FragmentCallBinding>(R.layout.fragment_call) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/cpr2u_android/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,13 @@
+package com.example.cpr2u_android.presentation.profile
+
+import android.os.Bundle
+import android.view.View
+import com.example.cpr2u_android.R
+import com.example.cpr2u_android.databinding.FragmentProfileBinding
+import com.example.cpr2u_android.presentation.base.BaseFragment
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
