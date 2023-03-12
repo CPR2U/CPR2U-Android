2a7b9f29934e84ebb8572308507017ce238ad9df
chaeyeong <shimcy2010@gmail.com>
2023-03-13 00:42:16 +0900

    [UI] #9 - 교육 자세연습 UI 구현

---
+++ app/src/main/java/com/example/cpr2u_android/presentation/education/PosePractice3Fragment.kt
@@ -0,0 +1,14 @@
+package com.example.cpr2u_android.presentation.education
+
+import android.os.Bundle
+import android.view.View
+import com.example.cpr2u_android.R
+import com.example.cpr2u_android.databinding.FragmentPosePractice3Binding
+import com.example.cpr2u_android.presentation.base.BaseFragment
+
+class PosePractice3Fragment :
+    BaseFragment<FragmentPosePractice3Binding>(R.layout.fragment_pose_practice_3) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/cpr2u_android/presentation/education/PosePracticeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cpr2u_android.presentation.education
+
+import android.os.Bundle
+import com.example.cpr2u_android.R
+import com.example.cpr2u_android.databinding.ActivityPosePracticeBinding
+import com.example.cpr2u_android.presentation.base.BaseActivity
+
+class PosePracticeActivity :
+    BaseActivity<ActivityPosePracticeBinding>(R.layout.activity_pose_practice) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
