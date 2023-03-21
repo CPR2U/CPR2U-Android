2adf4dd66fd6fbf954667e1ee8c06b312cf2fbd9
chaeyeong <shimcy2010@gmail.com>
2023-03-21 15:26:50 +0900

    [FEAT] #14 - 교육탭 강의 : 특정 시간 지나면 팝업

--- app/src/main/java/com/example/cpr2u_android/presentation/education/EducationFragment.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/education/EducationFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cpr2u_android.presentation.education
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.example.cpr2u_android.R
@@ -9,5 +10,9 @@ import com.example.cpr2u_android.presentation.base.BaseFragment
 class EducationFragment : BaseFragment<FragmentEducationBinding>(R.layout.fragment_education) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.clLecture.setOnClickListener {
+            startActivity(Intent(requireContext(), LectureActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/cpr2u_android/presentation/education/LectureActivity.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/education/LectureActivity.kt
@@ -1,16 +1,49 @@
 package com.example.cpr2u_android.presentation.education
 
+import android.app.Dialog
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.WindowManager
 import android.webkit.WebViewClient
+import androidx.databinding.DataBindingUtil
 import com.example.cpr2u_android.R
 import com.example.cpr2u_android.databinding.ActivityLectureBinding
+import com.example.cpr2u_android.databinding.DialogQuizBinding
 import com.example.cpr2u_android.presentation.base.BaseActivity
+import timber.log.Timber
 
 class LectureActivity : BaseActivity<ActivityLectureBinding>(R.layout.activity_lecture) {
+    private var start: Long = 0
+    private var end: Long = 0
+    private var sum: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.webView.webViewClient = WebViewClient()
         // TODO : 추후 학습 영상 링크 url로 변경 필요
         binding.webView.loadUrl("https://www.naver.com/")
+        Timber.d("## on Start -> $start")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val dialog = Dialog(this)
+            val binding = DataBindingUtil.inflate<DialogQuizBinding>(
+                LayoutInflater.from(this),
+                R.layout.dialog_quiz,
+                null,
+                false,
+            )
+            binding.buttonFinish.setOnClickListener {
+                dialog.dismiss()
+                finish()
+            }
+            dialog.setContentView(binding.root)
+            dialog.window?.setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+            )
+
+            dialog.show()
+        }, 5000L)
     }
 }
