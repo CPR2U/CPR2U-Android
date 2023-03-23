b0d0951f3fe1b74c9d21f7a2b7045d5efa50b219
chaeyeong <shimcy2010@gmail.com>
2023-03-24 00:41:59 +0900

    [UI] #19 - 호출탭 : 처음 위치 서울로 띄운 후, 현재 위치 버튼 누를 시 현재 위치 마커 띄움

--- app/src/main/java/com/example/cpr2u_android/presentation/call/CallFragment.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/call/CallFragment.kt
@@ -1,7 +1,10 @@
 package com.example.cpr2u_android.presentation.call
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,15 +12,23 @@ import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.cpr2u_android.R
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class CallFragment : Fragment(), OnMapReadyCallback {
+class CallFragment : Fragment(), OnMapReadyCallback, LocationListener {
     private lateinit var map: GoogleMap
     private val locationPermissionCode = 100
     lateinit var mapFragment: MapView
 
+    private lateinit var mMap: GoogleMap
+    private lateinit var mLocationManager: LocationManager
+    private lateinit var mMarker: Marker
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,8 +46,6 @@ class CallFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -52,18 +61,31 @@ class CallFragment : Fragment(), OnMapReadyCallback {
             )
             return
         }
-    }
+        mMap = googleMap ?: return
+        mMap.isMyLocationEnabled = true
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray,
-    ) {
-        if (requestCode == locationPermissionCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                onMapReady(map)
-            }
+        // 처음 화면 서울 -> 현재 위치 버튼 눌러야 내위치 이동
+        val posGps: LatLng = LatLng(37.5642135, 127.0016985)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posGps, 15f))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15f), 2000, null)
+
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+
+        // Get the user's current location and mark it on the map
+        mLocationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
         }
+//        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, this)
     }
 
     override fun onStart() {
@@ -84,5 +106,31 @@ class CallFragment : Fragment(), OnMapReadyCallback {
     override fun onDestroy() {
         super.onDestroy()
         mapFragment
+    }
+
+    override fun onLocationChanged(location: Location) {
+        location ?: return
+
+        // Add a marker for the user's current location
+        if (::mMarker.isInitialized) {
+            mMarker.remove()
+        }
+        mMarker =
+            mMap.addMarker(
+                MarkerOptions().position(LatLng(location.latitude, location.longitude))
+                    .title("Current Location"),
+            )!!
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    location.latitude,
+                    location.longitude,
+                ),
+                15f,
+            ),
+        )
+
+        // Stop updating the user's location to save battery
+//        mLocationManager.removeUpdates(this)
     }
 }
