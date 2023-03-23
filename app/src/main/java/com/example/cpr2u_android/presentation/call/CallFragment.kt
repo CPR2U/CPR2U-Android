06cf975308cb5e6ff763a5fb0df1b78b3deca3ed
chaeyeong <shimcy2010@gmail.com>
2023-03-23 22:42:20 +0900

    [UI] #19 - 호출탭 : 지도 띄우기

--- app/src/main/java/com/example/cpr2u_android/presentation/call/CallFragment.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/call/CallFragment.kt
@@ -1,13 +1,88 @@
 package com.example.cpr2u_android.presentation.call
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.example.cpr2u_android.R
-import com.example.cpr2u_android.databinding.FragmentCallBinding
-import com.example.cpr2u_android.presentation.base.BaseFragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class CallFragment : BaseFragment<FragmentCallBinding>(R.layout.fragment_call) {
+class CallFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+    private val locationPermissionCode = 100
+    lateinit var mapFragment: MapView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_call, container, false)
+        mapFragment = view.findViewById<MapView>(R.id.mapFragment)
+        mapFragment.onCreate(savedInstanceState)
+        mapFragment.getMapAsync(this)
+        return view
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode,
+            )
+            return
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onMapReady(map)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapFragment.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapFragment.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapFragment.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapFragment
     }
 }
