2a3f8d9949fa6e4f68ae82c23d0dc5d2b45fffcb
chaeyeong <shimcy2010@gmail.com>
2023-03-30 08:28:48 +0900

    [UI] #29 - 출동 신고하기 UI 구현

---
+++ app/src/main/java/com/example/cpr2u_android/presentation/call/DispatchReportActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cpr2u_android.presentation.call
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DispatchReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dispatch_report)
+    }
+}
