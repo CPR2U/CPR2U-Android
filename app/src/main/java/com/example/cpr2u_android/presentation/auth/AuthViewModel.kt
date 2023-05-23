ec959104504d2ecf7f4199c4d95b89d336efa061
chaeyeong <shimcy2010@gmail.com>
2023-05-23 16:28:49 +0900

    [FEAT] #40 - 로그아웃 dialog 구현

--- app/src/main/java/com/example/cpr2u_android/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/cpr2u_android/presentation/auth/AuthViewModel.kt
@@ -8,6 +8,9 @@ import com.example.cpr2u_android.data.model.request.auth.RequestLogin
 import com.example.cpr2u_android.data.model.request.auth.RequestSignUp
 import com.example.cpr2u_android.data.sharedpref.CPR2USharedPreference
 import com.example.cpr2u_android.domain.repository.auth.AuthRepository
+import com.example.cpr2u_android.util.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -31,6 +34,9 @@ class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
 
     private val _isSuccess = MutableLiveData<Boolean>()
     var isSuccess: LiveData<Boolean> = _isSuccess
+
+    private val _logoutUIState = MutableStateFlow<UiState<Boolean>>(UiState.Loading)
+    val logoutUIState: StateFlow<UiState<Boolean>> = _logoutUIState
 
     fun setPhoneNumber(phoneNumber: String) {
         Timber.d("### set phone number -> $phoneNumber")
@@ -114,8 +120,10 @@ class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
             CPR2USharedPreference.setAccessToken("")
             CPR2USharedPreference.setRefreshToken("")
             Timber.d("post-logout-success")
+            _logoutUIState.emit(UiState.Success(true))
         }.onFailure {
             Timber.d("post-logout-fail -> $it")
+            _logoutUIState.emit(UiState.Failure("$it"))
         }
     }
 }
