be0949ba4c2f9fda3f702174604b0ea7641e251c
chaeyeong <shimcy2010@gmail.com>
2023-05-25 21:59:52 +0900

    [FEAT] #41 - FCM intent 전달 로직

--- app/src/main/java/com/example/cpr2u_android/util/FirebaseService.kt
+++ app/src/main/java/com/example/cpr2u_android/util/FirebaseService.kt
@@ -3,7 +3,7 @@ package com.example.cpr2u_android.util
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.os.PowerManager
@@ -16,6 +16,7 @@ import com.google.firebase.messaging.RemoteMessage
 import org.json.JSONObject
 import timber.log.Timber
 import java.util.Date
+
 
 class FirebaseService : FirebaseMessagingService() {
     var jsonType: String = ""
@@ -32,7 +33,7 @@ class FirebaseService : FirebaseMessagingService() {
         // TODO :: 전달받은 리모트 메시지를 처리
 
         val pm =
-            getSystemService(Context.POWER_SERVICE) as PowerManager
+            getSystemService(POWER_SERVICE) as PowerManager
 
         @SuppressLint("InvalidWakeLockTag")
         val wakeLock =
@@ -43,52 +44,70 @@ class FirebaseService : FirebaseMessagingService() {
             )
         wakeLock.acquire(3000)
         wakeLock.release()
-        // Wake up screen
-        // Wake up screen
 
-        Log.d("MyFcmService", "Notification Title :: ${remoteMessage.notification?.title}")
-        Log.d("MyFcmService", "Notification Body :: ${remoteMessage.notification?.body}")
-        Log.d("MyFcmService", "Notification Data :: ${remoteMessage.data}")
-        /*
-        Notification Title :: CPR Angel의 출동이 필요합니다.
-        Notification Body :: 경기도 화성시 송동 산49-57
-2023-05-22 20:46:41.696  6977-7350  MyFcmService            com.example.cpr2u_android            D  Notification Data :: {call=67, type=1}
-2023-05-22 20:46:41.696  6977-7350  FCM LOG                 com.example.cpr2u_android            D  From: 260439878640
-2023-05-22 20:46:41.696  6977-7350  FCM LOG TYPE            com.example.cpr2u_android            D  Message data payload: 1
-2023-05-22 20:46:41.697  6977-7350  FirebaseService         com.example.cpr2u_android            D  fcm >> 2
-2023-05-22 20:46:41.697  6977-7350  JSON OBJECT             com.example.cpr2u_android            E  {"call":"67","type":"1"}
-2023-05-22 20:46:41.697  6977-7350  FirebaseService         com.example.cpr2u_android            D  JSON OBJECT Type -> 1
-         */
-
-        Log.d("FCM LOG", "From: ${remoteMessage.from}")
+        Timber.tag("MyFcmService").d("Notification :: " + remoteMessage)
+        Timber.tag("MyFcmService").d("Notification :: " + remoteMessage.data)
 
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d("FCM LOG TYPE", "Message data payload: ${remoteMessage.data["type"]}")
+
             remoteMessage.data?.let {
+                val params: Map<String, String> = remoteMessage.data
+                val jsonObject = JSONObject(params)
+                Timber.tag("JSON OBJECT").e(jsonObject.toString())
+
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                val pendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                )
+                showNotification(pendingIntent)
+
+                // E/JSON OBJECT: {"call":"228","type":"1"}
+//                val type = jsonObject.getString("type")
+//                val callId = jsonObject.getString("call")
+//                if (type == "1") {
+//                    // call 변수 안에 있는 call id를 바로 띄워야함
+//                    Timber.d("callId -> $callId")
+//                    val dispatchIntent = Intent(this, MainActivity::class.java).apply {
+//                        flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+//
+//                        // putIntentExtras
+////                        putExtra("callId", callId)
+//                    }
+//                    dispatchIntent.putExtra("callId", callId)
+//
+//                    val pIntent = PendingIntent.getActivity(
+//                        this,
+//                        0,
+//                        dispatchIntent,
+//                        PendingIntent.FLAG_UPDATE_CURRENT,
+//                    )
+//                    showNotification(pIntent)
+//                } else {
+//                    val intent = Intent(this, MainActivity::class.java)
+//                    val pIntent = PendingIntent.getActivity(
+//                        this,
+//                        0,
+//                        intent,
+//                        PendingIntent.FLAG_UPDATE_CURRENT,
+//                    )
+//                    showNotification(pIntent)
+//                }
+
+//                Timber.d("JSON OBJECT Type -> $type")
+//                jsonType = type
+
                 Timber.d("data 전달 받음 ")
-                showNotification()
             }
         }
-
-        val params: Map<String, String> = remoteMessage.data
-        Timber.d("fcm >> ${remoteMessage.priority}")
-        val jsonObject = JSONObject(params)
-        Timber.tag("JSON OBJECT").e(jsonObject.toString())
-
-        // E/JSON OBJECT: {"call":"228","type":"1"}
-        val type = jsonObject.getString("type")
-        if (type == "1") {
-            // call 변수 안에 있는 call id를 바로 띄워야함
-//            val intent = Intent(this, MainActivity::class.java)
-//            val pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
-
-        Timber.d("JSON OBJECT Type -> $type")
-        jsonType = type
     }
 
-    private fun showNotification() {
+    private fun showNotification(pIntent: PendingIntent) {
         /*
         1. 알림(노티피케이션)을 누를 시 이동할 화면을 정하여 Intent 객체를 생성한다.
         2. 이 인텐트는 당장 실행되는 것이 아니라 지연되므로 PendingIntent를 생성하여 위 Intent를 담는다.
@@ -98,18 +117,16 @@ class FirebaseService : FirebaseMessagingService() {
 
         데이터를 백그라운드, 포그라운드에서 모두 접근하여 활용하려면 서버측에서 전송 시 노티피케이션 부분을 제거하고 데이터만 포함하도록 해야한다.
          */
-
-        val intent = Intent(this, MainActivity::class.java)
-//        val pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
         val channelId = "channelId"
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle("환자 발생")
-            .setContentText(jsonType)
-//            .setContentIntent(pIntent)
+            .setContentText("환자 위치")
+            .setFullScreenIntent(null, true)
+            .setAutoCancel(true)
+            .setContentIntent(pIntent)
 
         getSystemService(NotificationManager::class.java).run {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
